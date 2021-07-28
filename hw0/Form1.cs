d776c7a50513e0b05226246946d0bac0d7e447fa
valiz <valiz@DESKTOP-ED381US>
2021-07-28 23:49:57 +0400

    Add project files.

---
+++ hw0/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Time = TimeZoneInfo.FindSystemTimeZoneById("Azerbaijan Standard Time");
+            DateTime strTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, Time);
+            Text = Convert.ToString(strTime);
+            BackgroundImage = Properties.Resources.baku;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var Time = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            DateTime strTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, Time);
+            Text = Convert.ToString(strTime);
+            BackgroundImage = Properties.Resources.london;
+        }
+    }
+}
---
+++ hw0_1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw0_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            if(sender is Button btn)
+            {
+                btn.Dispose();
+                Random random = new Random();
+                int x = random.Next(Width - 120);
+                int y = random.Next(Height - 60);
+
+                Button button = new Button
+                {
+                    Size = new Size(120, 60),
+                    Location = new Point(x, y),
+                    Text = "Don't touch this",
+                };
+
+                button.Click += button1_MouseHover;
+
+                Controls.Add(button);
+
+            }
+        }
+    }
+}
